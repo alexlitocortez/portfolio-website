6a07b87ffa1812b55b830ba4da23f9746b64317c
Alex Cortez <acim650@gmail.com>
2021-12-05 11:20:36 -0800

    src/components

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div>
+            <nav className='NavBar'>
+                <ul>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
